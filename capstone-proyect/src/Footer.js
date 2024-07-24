b2d652f87f8556ee2b105b19e9a48bec99a5f98c
DaNiArI <daniel.ariguel@hotmail.com>
2024-07-24 13:37:26 -0300

    semantic tags and ogp

---
+++ capstone-proyect/src/Footer.js
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container">
+        <p>&copy; 2022 Your Company. All rights reserved.</p>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
